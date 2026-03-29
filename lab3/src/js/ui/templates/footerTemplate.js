39a9412e743655d56692a9bfc1b554ca60bab7b8
Anastasiazzzz <stce.fr@gmail.com>
2026-03-29 15:24:31 +0500

    add missing files

--- lab3/src/js/ui/templates/footerTemplate.js
+++ lab3/src/js/ui/templates/footerTemplate.js
@@ -1,18 +1,3 @@
-export const createFooterColumnTemplate = ({ title, items }) => `
-    <div class="footer__block">
-        <h3 class="footer__header">${title}</h3>
-        <aside class="footer__menu">
-            <ul class="menu">
-                ${items.map(item => `
-                    <li class="menu__item">
-                        <a href="#" class="item__link">${item}</a>
-                    </li>
-                `).join("")}
-            </ul>
-        </aside>
-    </div>
-`;
-
 export const footerTemplate = ({ 
     mainHeader, 
     mainButtonTitle, 
@@ -27,20 +12,24 @@ export const footerTemplate = ({
   const contactsTemplate = createFooterColumnTemplate(contactsCol);
 
   return `
-    <div class="farewell_section__top">
+    <div class="footer_section__top">
         <h2 class="top__header">${mainHeader}</h2>
-        <button class="top__button">${mainButtonTitle}</button>
+        <button class="top__button" type="button">${mainButtonTitle}</button>
     </div>
-    <div class="farewell_section__bottom">
+    <div class="footer_section__bottom">
         <div class="footer__logo_block">
             <img class="footer__logo" src="${logo.src}" alt="${logo.alt}" />
-            <p class="footer__description">${brandInfo.address} ${brandInfo.rights}</p>
+            <p class="footer__description">${brandInfo.address}</p>
+            <p class="footer__description">${brandInfo.rights}</p>
         </div>
         <div class="footer__links">
             ${linksTemplate}
             ${companyTemplate}
             ${contactsTemplate}
         </div>
+    </div>
+    <div class="footer_section__law">
+        <p class="law__text">© 2021 GPT-3. Все права защищены.</p>
     </div>
   `;
 };
