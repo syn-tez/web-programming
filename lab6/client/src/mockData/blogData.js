6fe2bfb6bae031b1a9da284c144f80cbc2588182
Anastasiazzzz <stce.fr@gmail.com>
2026-04-15 21:48:32 +0500

    add mockData

---
+++ lab6/client/src/mockData/blogData.js
@@ -0,0 +1,48 @@
+import blog1 from "../assets/img/blogs/blog_1.svg";
+import blog2 from "../assets/img/blogs/blog_2.svg";
+import blog3 from "../assets/img/blogs/blog_3.svg";
+import blog4 from "../assets/img/blogs/blog_4.svg";
+import blog5 from "../assets/img/blogs/blog_5.svg";
+
+export const header = "Многое Происходит, Мы Ведем об Этом Блог.";
+
+export const articles = [
+  {
+    image: blog1,
+    date: "Сент 26, 2021",
+    title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+    isLarge: true,
+  },
+  {
+    image: blog2,
+    date: "Сент 26, 2021",
+    title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+  },
+  {
+    image: blog4,
+    date: "Сент 26, 2021",
+    title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+  },
+  {
+    image: blog3,
+    date: "Сент 26, 2021",
+    title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+  },
+  {
+    image: blog5,
+    date: "Сент 26, 2021",
+    title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
+    linkText: "Читать полную статью",
+  },
+];
+
+const blogData = {
+  header,
+  articles,
+};
+
+export default blogData;
---
+++ lab6/client/src/mockData/showcaseData.js
@@ -0,0 +1,24 @@
+import showcaseSvg from "../assets/img/showcase.svg";
+
+export const illustration = {
+  src: showcaseSvg,
+  alt: "showcase",
+};
+
+export const eyebrow = "Запросить ранний доступ";
+
+export const title = "Превосходя все ожидания";
+
+export const description = "Однако кровать для помощи в путешествии неприятна. Не мысли все осуществляют благословение. Снисхождение ко всему, радость, изменение бурной привязанности. Вечеринку мы лет на заказ разрешили.";
+
+export const bottomCta = "Запросить ранний доступ";
+
+export const showcaseData = {
+  illustration,
+  eyebrow,
+  title,
+  description,
+  bottomCta,
+};
+
+export default showcaseData;
