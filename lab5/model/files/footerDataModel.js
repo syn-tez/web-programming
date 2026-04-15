4911b1ddae30a20625056a8dc27dcfc399edfd91
Anastasiazzzz <stce.fr@gmail.com>
2026-04-15 16:40:44 +0500

    add model

---
+++ lab5/model/files/footerDataModel.js
@@ -0,0 +1,25 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
+
+const filePath = join(__dirname, "..", "..", "db", "files", "footerData.json");
+
+const getFooterDataModel = () => {
+    try {
+        const file = readFileSync(filePath, "utf-8");
+        return JSON.parse(file);
+    } catch (error) {
+        console.log(error);
+        throw new Error("Не получилось прочитать данные из файла footerData.json");
+    }
+    };
+
+    const postFooterDataModel = (data) => {
+    try {
+        writeFileSync(filePath, data);
+    } catch (error) {
+        console.log(error);
+        throw new Error("Не получилось записать данные в файл footerData.json");
+    }
+};
+
+module.exports = { getFooterDataModel, postFooterDataModel };
