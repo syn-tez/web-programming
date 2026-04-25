793f4840ccd20577556e46c42b87005908d90a34
Anastasiazzzz <stce.fr@gmail.com>
2026-04-26 00:12:06 +0500

    add folder mockData

---
+++ lab8/client/src/mockData/showcaseData.js
@@ -0,0 +1,22 @@
+export const illustration = {
+  src: "./assets/img/showcase.svg",
+  alt: "showcase",
+};
+
+export const eyebrow = "Запросить ранний доступ";
+
+export const title = "Превосходя все ожидания";
+
+export const description = "Однако кровать для помощи в путешествии неприятна. Не мысли все осуществляют благословение. Снисхождение ко всему, радость, изменение бурной привязанности. Вечеринку мы лет на заказ разрешили.";
+
+export const bottomCta = "Запросить ранний доступ";
+
+export const showcaseData = {
+  illustration,
+  eyebrow,
+  title,
+  description,
+  bottomCta,
+};
+
+export default showcaseData;
